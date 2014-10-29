159b1308d62136e98e30d26ab58fe2f3c7e82509
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-10-29 16:54:18 -0700

    Adds .env API access

---
+++ server/yelpkeys.js
@@ -0,0 +1,8 @@
+var keys = {
+	yelpConsumerKey: process.env.YELP_CONSUMER_KEY,
+	yelpConsumerSecret: process.env.YELP_CONSUMER_SECRET,
+	yelpToken: process.env.YELP_TOKEN, 
+	yelpTokenSecret: process.env.YELP_TOKEN_SECRET
+}
+
+module.exports = keys; 
