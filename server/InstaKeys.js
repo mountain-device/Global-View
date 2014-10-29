440a2543a2048a27ac39f8c47ec94387f70eaab1
Ryo Osawa <ryojiosawa@gmail.com>
2014-10-29 11:31:34 -0700

    Externalize Twitter and Instagram API config values

--- server/InstaKeys.js
+++ server/InstaKeys.js
@@ -5,9 +5,3 @@ var keys = {
 };
 
 module.exports = keys;
-
-/*module.exports.keys = {
-  instaClientID: 'a6fe1a07d99546a3b3e269fa9b431771',
-  instaClientKey: '59e723704aa941c2a02e24551173286e',
-  instaToken: '1958337.a6fe1a0.f20a93375cbf4cb1beb581c3786f2328'
-};*/
